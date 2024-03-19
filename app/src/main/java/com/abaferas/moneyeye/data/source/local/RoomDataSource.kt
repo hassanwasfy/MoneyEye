37f71c51b5fdb8f13be62f18e0bdbadb3ff5f42f
Hassan Wasfy <hassanwasfy7@gmail.com>
2024-03-19 02:45:44 +0200

    implement room data source

--- app/src/main/java/com/abaferas/moneyeye/data/source/local/RoomDataSource.kt
+++ app/src/main/java/com/abaferas/moneyeye/data/source/local/RoomDataSource.kt
@@ -1,7 +1,99 @@
 package com.abaferas.moneyeye.data.source.local
 
+import com.abaferas.moneyeye.data.models.dao.LocalAccountDao
+import com.abaferas.moneyeye.data.models.dao.LocalCategoriesDao
+import com.abaferas.moneyeye.data.models.dao.LocalCategoryDao
+import com.abaferas.moneyeye.data.models.dao.LocalTransactionDao
+import com.abaferas.moneyeye.data.models.dao.LocalUserDao
+import com.abaferas.moneyeye.data.models.local.LocalAccount
+import com.abaferas.moneyeye.data.models.local.LocalCategories
+import com.abaferas.moneyeye.data.models.local.LocalCategory
+import com.abaferas.moneyeye.data.models.local.LocalTransaction
+import com.abaferas.moneyeye.data.models.local.LocalUser
 import com.abaferas.moneyeye.domain.source.local.LocalDataSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class RoomDataSource: LocalDataSource {
+class RoomDataSource @Inject constructor(
+    private val accountDao: LocalAccountDao,
+    private val transactionDao: LocalTransactionDao,
+    private val userDao: LocalUserDao,
+    private val categoriesDao: LocalCategoriesDao,
+    private val categoryDao: LocalCategoryDao
+): LocalDataSource {
+    override suspend fun insertNewAccount(account: LocalAccount) {
+        accountDao.insertNewAccount(account)
+    }
 
+    override suspend fun updateExistingAccount(account: LocalAccount) {
+        accountDao.updateExistingAccount(account)
+    }
+
+    override suspend fun deleteAccount(account: LocalAccount) {
+        accountDao.deleteAccount(account)
+    }
+
+    override fun getAllUserAccounts(userId: Long): Flow<List<LocalAccount>> {
+        return accountDao.getAllUserAccounts(userId)
+    }
+
+    override suspend fun insertRecordCategories(localCategories: LocalCategories) {
+        categoriesDao.insert(localCategories)
+    }
+
+    override suspend fun updateRecordCategories(localCategories: LocalCategories) {
+        categoriesDao.update(localCategories)
+    }
+
+    override suspend fun deleteRecordCategories(localCategories: LocalCategories) {
+        categoriesDao.delete(localCategories)
+    }
+
+    override fun getRecordCategories(transactionId: Long): Flow<List<String>> {
+        return categoriesDao.getRecordCategories(transactionId)
+    }
+
+    override suspend fun insertCategory(localCategory: LocalCategory) {
+        categoryDao.insert(localCategory)
+    }
+
+    override suspend fun updateCategory(localCategory: LocalCategory) {
+        categoryDao.update(localCategory)
+    }
+
+    override suspend fun deleteCategory(localCategory: LocalCategory) {
+        categoryDao.delete(localCategory)
+    }
+
+    override fun getAllCategories(): Flow<List<LocalCategory>> {
+        return categoryDao.getAllCategories()
+    }
+
+    override suspend fun insertRecord(localTransaction: LocalTransaction) {
+        transactionDao.insert(localTransaction)
+    }
+
+    override suspend fun updateRecord(localTransaction: LocalTransaction) {
+        transactionDao.update(localTransaction)
+    }
+
+    override suspend fun deleteRecord(localTransaction: LocalTransaction) {
+        transactionDao.delete(localTransaction)
+    }
+
+    override fun getAllAccountRecords(accountId: Long): Flow<List<LocalTransaction>> {
+        return transactionDao.getAllAccountRecords(accountId)
+    }
+
+    override suspend fun insertUser(localUser: LocalUser) {
+        userDao.insert(localUser)
+    }
+
+    override suspend fun updateUser(localUser: LocalUser) {
+        userDao.update(localUser)
+    }
+
+    override suspend fun deleteUser(localUser: LocalUser) {
+        userDao.delete(localUser)
+    }
 }
--- app/src/main/java/com/abaferas/moneyeye/domain/source/local/LocalDataSource.kt
+++ app/src/main/java/com/abaferas/moneyeye/domain/source/local/LocalDataSource.kt
@@ -1,5 +1,37 @@
 package com.abaferas.moneyeye.domain.source.local
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.abaferas.moneyeye.data.models.local.LocalAccount
+import com.abaferas.moneyeye.data.models.local.LocalCategories
+import com.abaferas.moneyeye.data.models.local.LocalCategory
+import com.abaferas.moneyeye.data.models.local.LocalTransaction
+import com.abaferas.moneyeye.data.models.local.LocalUser
+import com.abaferas.moneyeye.domain.utils.Constants
+import kotlinx.coroutines.flow.Flow
+
 interface LocalDataSource {
 
+    suspend fun insertNewAccount(account: LocalAccount)
+    suspend fun updateExistingAccount(account: LocalAccount)
+    suspend fun deleteAccount(account: LocalAccount)
+    fun getAllUserAccounts(userId: Long): Flow<List<LocalAccount>>
+    suspend fun insertRecordCategories(localCategories: LocalCategories)
+    suspend fun updateRecordCategories(localCategories: LocalCategories)
+    suspend fun deleteRecordCategories(localCategories: LocalCategories)
+    fun getRecordCategories(transactionId: Long): Flow<List<String>>
+    suspend fun insertCategory(localCategory: LocalCategory)
+    suspend fun updateCategory(localCategory: LocalCategory)
+    suspend fun deleteCategory(localCategory: LocalCategory)
+    fun getAllCategories(): Flow<List<LocalCategory>>
+    suspend fun insertRecord(localTransaction: LocalTransaction)
+    suspend fun updateRecord(localTransaction: LocalTransaction)
+    suspend fun deleteRecord(localTransaction: LocalTransaction)
+    fun getAllAccountRecords(accountId: Long): Flow<List<LocalTransaction>>
+    suspend fun insertUser(localUser: LocalUser)
+    suspend fun updateUser(localUser: LocalUser)
+    suspend fun deleteUser(localUser: LocalUser)
 }
